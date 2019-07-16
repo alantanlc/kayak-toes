60d13c483c47d9691a5a7d6033e89cb24fdef468
Alan Tan <alanwuha91@gmail.com>
2019-07-16 22:47:21 +0800

    added leaderboard and reference mock data, filter leaderboard by category

--- kt-angular/src/app/leaderboard/leaderboard.component.ts
+++ kt-angular/src/app/leaderboard/leaderboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
-import { leaderboard } from '../mock';
+import { leaderboard, reference } from '../mock';
 
 @Component({
   selector: 'app-leaderboard',
@@ -12,20 +12,24 @@ import { leaderboard } from '../mock';
 export class LeaderboardComponent implements OnInit {
   displayedColumns: string[] = ['affiliate', 'athlete', /*'boat', 'datetime',*/ 'result'];
   dataSource: MatTableDataSource<any>;
+  categories: any[];
 
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor() { }
 
   ngOnInit() {
+    this.categories = reference.filter(ref => ref.category === 'category');
     this.dataSource = new MatTableDataSource(leaderboard.filter(record => record.category === '200m'));
     this.dataSource.sort = this.sort;
   }
 
-  filter(category: string) {
-    console.log(category);
+  filterByCategory(category: string) {
     this.dataSource.data = leaderboard.filter(record => record.category === category);
-    console.log(this.dataSource.data);
+  }
+
+  isFirstCategory(category: any) {
+    return this.categories.indexOf(category) === 0;
   }
 
 }
