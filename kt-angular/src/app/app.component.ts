f890154c960489c3bd6da480657c0525115a0f45
Alan Tan <alanwuha91@gmail.com>
2019-07-16 18:22:30 +0800

    update app component layout for mobile and tablet

--- kt-angular/src/app/app.component.ts
+++ kt-angular/src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnDestroy {
   private _mobileQueryListener: () => void;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQuery = media.matchMedia('(max-width: 1140px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
